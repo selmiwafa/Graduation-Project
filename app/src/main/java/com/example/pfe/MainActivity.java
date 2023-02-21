95457059736d5aeb9975bda3d74f818ad03c8560
you@example.com <you@example.com>
2023-02-21 12:26:15 +0100

    update

--- app/src/main/java/com/example/pfe/MainActivity.java
+++ app/src/main/java/com/example/pfe/MainActivity.java
@@ -3,15 +3,20 @@ package com.example.pfe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
         setContentView(R.layout.activity_main);
+
     }
 
     public void OpenSignupPage(View view) {
