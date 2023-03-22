ac0ad621782488bf0ac748846de9d70ee94c3a44
Wafa Selmi <58610582+selmiwafa@users.noreply.github.com>
2023-03-22 12:57:17 +0100

    Update HomepageActivity.java

--- app/src/main/java/com/example/pfe/HomepageActivity.java
+++ app/src/main/java/com/example/pfe/HomepageActivity.java
@@ -2,6 +2,7 @@ package com.example.pfe;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +16,8 @@ public class HomepageActivity extends AppCompatActivity {
         getWindow().setStatusBarColor(Color.TRANSPARENT);
         setContentView(R.layout.activity_home_page);
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+    }
+    public void logout(View view) {
+        SharedPrefManager.getInstance(getApplicationContext()).logout();
     }
 }
