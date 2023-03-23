459d3dd6cae440fd58c5683a3b9e04cd4dea267b
amina <bouabidiamina4@gmail.com>
2023-03-23 13:58:01 +0100

    user interface

---
+++ app/src/main/java/com/example/pfe/ManageAccountActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pfe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ManageAccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manage_account)
+    }
+}
