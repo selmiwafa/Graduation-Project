d3d36d1e49bd20acb9956a2a513d283549d04493
you@example.com <you@example.com>
2023-03-25 04:16:31 +0100

    Navigation drawer menu

--- app/src/main/java/com/example/pfe/HomepageActivity.java
+++ app/src/main/java/com/example/pfe/HomepageActivity.java
@@ -9,42 +9,24 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
 
-import java.util.Objects;
-
-public class HomepageActivity extends AppCompatActivity {
+public class HomepageActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private AlertDialog.Builder dialogBuilder;
     private AlertDialog dialog;
     TextView username, detail_email;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
-    ActionBarDrawerToggle drawerToggle;
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        onBackPressed();
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,41 +36,42 @@ public class HomepageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home_page);
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
 
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(myToolbar);
         drawerLayout = findViewById(R.id.drawerlayout);
         navigationView = findViewById(R.id.nav_menu);
-        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
-        drawerLayout.addDrawerListener(drawerToggle);
-        drawerToggle.syncState();
-        Objects.requireNonNull(getSupportActionBar()).setDisplayShowHomeEnabled(true);
-        navigationView.setNavigationItemSelectedListener((NavigationView.OnNavigationItemSelectedListener) item -> {
-            switch (item.getItemId()) {
-                case R.id.patient1:
-                {
-                    Toast.makeText(HomepageActivity.this, "Hi", Toast.LENGTH_SHORT).show();
-                }
-                case R.id.home:
-                {
-                    Toast.makeText(HomepageActivity.this, "Home", Toast.LENGTH_SHORT).show();
-                }
-                break;
-            }
-            return false;
-        });
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle;
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
     }
 
-    public void setSupportActionBar(Toolbar myToolbar) {
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     public void logout(View view) {
         SharedPrefManager.getInstance(getApplicationContext()).logout();
     }
-    public void OpenManageAccount (View view) {
-        startActivity(new Intent( HomepageActivity.this, ManageAccountActivity.class));
+
+    public void OpenManageAccount(View view) {
+        startActivity(new Intent(HomepageActivity.this, ManageAccountActivity.class));
     }
+
     public void createUserDialog(View view) {
-        dialogBuilder=new AlertDialog.Builder(this);
+        dialogBuilder = new AlertDialog.Builder(this);
         final View contactPopupView=getLayoutInflater().inflate(user_details,null);
         dialogBuilder.setView(contactPopupView);
         dialog=dialogBuilder.create();
@@ -99,18 +82,12 @@ public class HomepageActivity extends AppCompatActivity {
         detail_email.setText(String.valueOf(SharedPrefManager.getInstance(getApplicationContext()).getUser().getEmail()));
     }
 
-    @Override
-    public void onBackPressed() {
-        if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START);
-        }
-        else
-        {
-            super.onBackPressed();
-        }
+    public void cancel(View view) {
+        dialog.dismiss();
     }
 
-    public void cancel (View view){
-        dialog.dismiss();
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
     }
 }
