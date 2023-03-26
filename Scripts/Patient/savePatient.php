72c6e5f58c5aad07c21904168df9fecb465b77c1
amina27Bouabidi <90248860+amina27Bouabidi@users.noreply.github.com>
2023-03-26 15:28:30 +0100

    Add files via upload

--- Scripts/Patient/savePatient.php
+++ Scripts/Patient/savePatient.php
@@ -4,34 +4,34 @@ $response=array();
 $data = json_decode(file_get_contents("php://input"));
 $returnData = [];
 
-if(isset($_GET["email"]) && isset($_GET["password"]))
+if(isset($_GET["email"]) && isset($_GET["patient_name"]))
 {
     $email=$_GET["email"];
-    $password=$_GET["password"];
+    $patient_name=$_GET["patient_name"];
 
-    $req=mysqli_query($cnx,"select * from user where email='$email'");
+    $req=mysqli_query($cnx,"select * from patients where user='$email'");
     if(mysqli_num_rows($req)>0)
     {
         $cur=mysqli_fetch_array($req);
-        if($password==$cur["password"])
+        if($patient_name==$cur["patient_name"])
         {
             $tmp=array();
-            $response["user"]=array();
-            $tmp["email"]=$cur["email"];
-            $tmp["name"]=$cur["name"];
-            $tmp["birthdate"]=$cur["birthdate"];
-            $tmp["password"]=$cur["password"];
-            $tmp["adress"]=$cur["adress"];
+            $response["patients"]=array();
+            $tmp["patient_name"]=$cur["patient_name"];
+            $tmp["user"]=$cur["user"];
+            $tmp["relationship"]=$cur["relationship"];
+            $tmp["age"]=$cur["age"];
+           
             
             $response["success"]=1;
-            $response["message"]="logged";
+            $response["message"]="patient saved";
             array_push($response["user"],$tmp);
             echo json_encode($response);
         }
         else
         {
             $response["success"]=0;
-            $response["message"]="wrong password";
+            $response["message"]="wrong patient_name";
             echo json_encode($response);
         }
         
@@ -39,7 +39,7 @@ if(isset($_GET["email"]) && isset($_GET["password"]))
     else
     {
         $response["success"]=0;
-        $response["message"]="no user found";
+        $response["message"]="no patient found";
         echo json_encode($response);
     }
 }
