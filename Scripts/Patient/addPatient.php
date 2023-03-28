6e9a0524a43faa3dd4cb98ee6d71bbc400f4b652
Wafa Selmi <58610582+selmiwafa@users.noreply.github.com>
2023-03-28 18:23:21 +0100

    Add files via upload

--- Scripts/Patient/addPatient.php
+++ Scripts/Patient/addPatient.php
@@ -5,26 +5,35 @@ include ("db_connect.php");
 $response=array();
 
 if(
-   isset($_GET["name"]) &&
+   isset($_GET["patient_name"]) &&
    isset($_GET["relationship"]) &&
-   isset($_GET["age"] ) &&
+   isset($_GET["patient_age"] ) &&
    isset($_GET["user"])
    )
    {
-      $name=$_GET["name"];
+      $name=$_GET["patient_name"];
       $relationship=$_GET["relationship"];
-      $age=$_GET["age"];
+      $age=$_GET["patient_age"];
       $user=$_GET["user"];
 
-      $req1=mysqli_query($cnx,"select * from patients where user='$user'");
-      if(mysqli_num_rows($req1)<2)
+      $req1=mysqli_query($cnx,"select patient_name, patient_age, relationship  from patients where user='$user'");
+      $number=mysqli_num_rows($req1);
+      if($number<2)
       {
-
          $req=mysqli_query($cnx,"insert into patients(patient_name,relationship,patient_age,user) values ('$name','$relationship','$age','$user')");
          if($req)
          {
+            $cur=mysqli_fetch_array($req1);
+            $tmp=array();
+            $response["user"]=array();
+            $tmp["patient_name"]=$cur["patient_name"];
+            $tmp["patient_age"]=$cur["patient_age"];
+            $tmp["relationship"]=$cur["relationship"];
+
             $response["success"]=1;
             $response["message"]="inserted!";
+            $response["number"]=$number+1;
+            array_push($response["user"],$tmp);
             echo json_encode($response);
          }
          else
@@ -37,7 +46,7 @@ if(
       else
          {
             $response["success"]=0;
-            $response["message"]="Maximum patients reached";
+            $response["message"]="You can only have 2 patients maximum!";
             echo json_encode($response);
             
          }
