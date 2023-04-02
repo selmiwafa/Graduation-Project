d0c52c26b0a6d8da5439480b0b6ab6327720cd9c
you@example.com <you@example.com>
2023-04-02 05:28:50 +0100

    AddPatientActivity + MyPatientsActivity

--- app/src/main/java/com/example/pfe/manage_patients/Patient1Activity.java
+++ app/src/main/java/com/example/pfe/manage_patients/Patient1Activity.java
@@ -1,9 +1,14 @@
 package com.example.pfe.manage_patients;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.pfe.MyPatientsActivity;
 import com.example.pfe.R;
 
 public class Patient1Activity extends AppCompatActivity {
@@ -11,6 +16,13 @@ public class Patient1Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
         setContentView(R.layout.activity_patient1);
+    }
+
+    public void OpenMypatients(View view) {
+        startActivity(new Intent(Patient1Activity.this, MyPatientsActivity.class));
     }
 }
--- app/src/main/java/com/example/pfe/manage_patients/Patient2Activity.java
+++ app/src/main/java/com/example/pfe/manage_patients/Patient2Activity.java
@@ -1,6 +1,8 @@
 package com.example.pfe.manage_patients;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +13,9 @@ public class Patient2Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
         setContentView(R.layout.activity_patient2);
     }
 }
